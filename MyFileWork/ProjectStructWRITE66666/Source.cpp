f9c273f5dd0c11dddfb7021fc0a38872dceb4f3b
DmytroKhoptian <48955602+DmytroKhoptian@users.noreply.github.com>
2022-06-08 23:25:18 +0300

    some change

--- MyFileWork/ProjectStructWRITE66666/Source.cpp
+++ MyFileWork/ProjectStructWRITE66666/Source.cpp
@@ -19,7 +19,7 @@ struct Student
 
 int main()
 {
-	Student student1 = { "Vova", 28 };
+	Student student1 = { "Vova", 281 };
 	Student student2 = { "Vasia", 12211 };
 
 	string filename = "E:\\MEGA\\Основы С++\\17Файлы\\MyFileWork\\ProjectStructWRITE66666\\struct.txt";
